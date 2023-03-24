ec81d3fe5cf26111fb9cde8a9981763a4693e1f5
Daykoo <me@daykoo.de>
2023-03-24 12:54:46 +0100

    Feature: Dashboard added

---
+++ dashboard/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BoardComponent } from './board/board.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path: 'board', component: BoardComponent},
+  {path: 'login', component: LoginComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
